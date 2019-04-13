9f3c0983bd5c5ea0b26993e0b884a561f5965061
abmurari <murari.abhinav@gmail.com>
2019-04-14 00:54:31 +0530

    Committing base code without comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+makeCacheMatrix <- function(x = matrix()) 
+  {
+    matrixInv <- NULL
+    setMat <- function(y)
+    {
+      x <<- y
+      matrixInv <<- NULL
+    }
+    getMat <- function() x
+    setInv <- function(solve) matrixInv <<- solve
+    getInv <- function() matrixInv
+    list(setMat = setMat, getMat = getMat, setInv = setInv, getInv = getInv)
+  }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+  {
         ## Return a matrix that is the inverse of 'x'
-}
+    matrixInv <- x$getInv()
+    if(!is.null(matrixInv))
+    {
+      message("getting cached matrix inverse")
+      return(matrixInv)
+    }
+    matrixData <- x$getMat()
+    matrixInv <- solve(matrixData, ...)
+    x$setInv(matrixInv)
+    matrixInv
+  }
